ea00e0e3cb982232d315d2a101d945d293629c0d
iocode <bulawake@gmail.com>
2017-09-11 16:01:28 +0800

    added first function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ok } from './components/helpers';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { currentCount: 0 }
+    }
+
+    timer() {
+        this.setState({
+            currentCount: this.state.currentCount + 1
+        })
+        // if (this.state.currentCount > 10) clearInterval(this.intervalId);
+    }
+
+    componentDidMount() {
+        this.intervalId = setInterval(this.timer.bind(this), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
+    }
+
+    render() {
+        return (
+            <div>{this.state.currentCount}</div>
+        );
   }
 }
 
