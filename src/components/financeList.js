9fed924038e961cb1369f4be12836c06265039d3
IOcode <bulawake@gmail.com>
2017-09-14 09:10:25 +0800

    Update financeList.js

--- src/components/financeList.js
+++ src/components/financeList.js
@@ -26,36 +26,3 @@ class FinanceList extends React.Component {
 }
 
 export default FinanceList
-// class App extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = { 
-//             currentCount: 0,
-//             goodsPerSecond: calculate(this.props.goodsPrice, this.props.monthlyIncoming)
-//         }
-//     }
-    
-//     timer() {
-//         this.setState({
-//             currentCount: this.state.currentCount + 1
-//         })
-//         // if (this.state.currentCount > 10) clearInterval(this.intervalId);
-//     }
-
-//     componentDidMount() {
-//         this.intervalId = setInterval(this.timer.bind(this), 1000);
-//     }
-
-//     componentWillUnmount() {
-//         clearInterval(this.intervalId);
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 <h1>OKKKK</h1>
-//                 <FinanceList persons={PERSONS} />
-//             </div>
-//         );
-//   }
-// }
